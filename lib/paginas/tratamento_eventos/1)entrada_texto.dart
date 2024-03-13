788e611cc6d40f37b2977246559e36eb176b9b02
graconlima <graconlima@yahoo.com.br>
2024-03-13 08:42:34 -0300

    Rename widgest_basicas.dart to widgets_basicas.dart

---
+++ lib/paginas/tratamento_eventos/1)entrada_texto.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+//Exemplo 1: lendo entrada de usuario e calculando IMC
+/*void main(){
+  double a = 1.70, p = 70.0;
+  runApp(MaterialApp(home: Scaffold(body: Column(
+      children: <Widget>[
+        TextField(
+            onChanged: (texto) {a = double.parse(texto); double imc = p/(a*a);print("IMC: $imc");},
+            decoration: InputDecoration(icon:Icon(Icons.accessibility), hintText: "Altura")
+        ),
+        TextField(
+            onChanged: (texto) {p = double.parse(texto); double imc = p/(a*a);print("IMC: $imc");},
+            decoration: InputDecoration(icon:Icon(Icons.scale), hintText: "Peso:")
+        )
+      ]
+  )))
+  );
+}*/
