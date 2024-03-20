ad773bb4e14772ddd8196f53113a939af1006b30
graconlima <graconlima@yahoo.com.br>
2024-03-20 12:34:10 -0300

    Rename widgest_basicas.dart to widgets_basicas.dart

--- lib/paginas/tratamento_eventos/2)botoes.dart
+++ lib/paginas/tratamento_eventos/2)botoes.dart
@@ -1,3 +1,4 @@
+import 'package:ddm_hibrido/dados_compartilhados/modelos/calculadora.dart';
 import 'package:flutter/material.dart';
 //Exemplo 1 - botoes
 /*void main(){
@@ -50,5 +51,30 @@ class IMC extends StatelessWidget{
 
   void calcular(){
     double imc = p/(a*a);print("IMC: $imc");
+  }
+}*/
+
+//Exemplo 3: IMC 2.0: calculando com botao (com modelo)
+/*void main(){
+  runApp(IMC());
+}
+
+class IMC extends StatelessWidget{
+  double a = 1.70, p = 70.0;
+
+  Widget build(BuildContext bc) {
+    return MaterialApp(home: Scaffold(body: Column(
+    children: <Widget>[
+      TextField(
+        onChanged: (texto) {a = double.parse(texto);},
+        decoration: InputDecoration(icon:Icon(Icons.accessibility), hintText: "Altura")
+      ),
+      TextField(
+        onChanged: (texto) {p = double.parse(texto);},
+        decoration: InputDecoration(icon:Icon(Icons.scale), hintText: "Peso:")
+      ),
+      FloatingActionButton(child: Text("IMC"), onPressed: (){print("${Calculadora.imc(a, p)}");})
+    ]
+    )));
   }
 }*/
