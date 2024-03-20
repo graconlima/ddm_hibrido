37b4dc385a5e0cb5010d8ae99625cff2c9f1f8c5
1958314 <graconlima@yahoo.com.br>
2024-03-20 10:34:28 -0300

    Revert "Rename widgest_basicas.dart to widgets_basicas.dart"
    This reverts commit 788e611c

--- lib/paginas/tratamento_eventos/1)entrada_texto.dart
+++ lib/paginas/tratamento_eventos/1)entrada_texto.dart
@@ -1,14 +1,8 @@
 import 'package:flutter/material.dart';
 //Exemplo 1: lendo entrada de usuario e calculando IMC
-<<<<<<< HEAD
-void main(){
-  double a = 1.70, p = 70.0;
-  runApp(MaterialApp(home: Scaffold(body: Center(child: Column(
-=======
 /*void main(){
   double a = 1.70, p = 70.0;
   runApp(MaterialApp(home: Scaffold(body: Column(
->>>>>>> origin/master
       children: <Widget>[
         TextField(
             onChanged: (texto) {a = double.parse(texto); double imc = p/(a*a);print("IMC: $imc");},
@@ -19,12 +13,6 @@ void main(){
             decoration: InputDecoration(icon:Icon(Icons.scale), hintText: "Peso:")
         )
       ]
-<<<<<<< HEAD
-  ))))
-  );
-}
-=======
   )))
   );
 }*/
->>>>>>> origin/master
--- lib/paginas/tratamento_eventos/2)botoes.dart
+++ lib/paginas/tratamento_eventos/2)botoes.dart
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
-import 'package:ddm_hibrido/dados_compartilhados/modelos/calculadora.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-=======
-import 'package:flutter/material.dart';
->>>>>>> origin/master
 //Exemplo 1 - botoes
 /*void main(){
   runApp(MinhaWidget());
@@ -31,51 +25,24 @@ class MinhaWidget extends StatelessWidget{
 }*/
 
 //Exemplo 2: IMC 2.0: calculando com botao
-<<<<<<< HEAD
-void main(){
-=======
 /*void main(){
->>>>>>> origin/master
   runApp(IMC());
 }
 
 class IMC extends StatelessWidget{
-<<<<<<< HEAD
-  double a = 1.70, p = 70.0, imc = 0.0;
-=======
   double a = 1.70, p = 70.0;
->>>>>>> origin/master
 
   Widget build(BuildContext bc) {
     return MaterialApp(home: Scaffold(body: Column(
     children: <Widget>[
       TextField(
         onChanged: (texto) {a = double.parse(texto);},
-<<<<<<< HEAD
-        decoration: InputDecoration(icon:Icon(Icons.accessibility), hintText: "Altura"),
-        keyboardType: TextInputType.number,
-        inputFormatters: [FilteringTextInputFormatter.allow(RegExp("[0-9.]+"))],
-=======
         decoration: InputDecoration(icon:Icon(Icons.accessibility), hintText: "Altura")
->>>>>>> origin/master
       ),
       TextField(
         onChanged: (texto) {p = double.parse(texto);},
         decoration: InputDecoration(icon:Icon(Icons.scale), hintText: "Peso:")
       ),
-<<<<<<< HEAD
-      FloatingActionButton(
-        child: Text("IMC"),
-        onPressed: () {
-          imc = Calculadora.imc(a, p);
-          print("IMC: ${Calculadora.imc(a, p)}");
-        }),
-      Text("$imc")
-    ]
-    )));
-  }
-}
-=======
       FloatingActionButton(child: Text("IMC"), onPressed: (){calcular();})
     ]
     )));
@@ -85,4 +52,3 @@ class IMC extends StatelessWidget{
     double imc = p/(a*a);print("IMC: $imc");
   }
 }*/
->>>>>>> origin/master
